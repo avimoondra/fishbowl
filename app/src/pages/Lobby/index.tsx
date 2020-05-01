0cf87124bf9ea63487b6bdfc7564466f9265531d
Will Tachau <wtachau@gmail.com>
2020-04-30 21:33:47 -0700

    add expansion panel

--- app/src/pages/Lobby/index.tsx
+++ app/src/pages/Lobby/index.tsx
@@ -2,12 +2,16 @@ import {
   Box,
   createStyles,
   Divider,
+  ExpansionPanel,
+  ExpansionPanelDetails,
+  ExpansionPanelSummary,
   Grid,
   makeStyles,
   Theme,
   Typography
 } from "@material-ui/core"
 import { grey } from "@material-ui/core/colors"
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore"
 import { CurrentGameContext } from "contexts/CurrentGame"
 import { CurrentPlayerContext, PlayerRole } from "contexts/CurrentPlayer"
 import { useTitleStyle } from "index"
@@ -45,55 +49,68 @@ function SettingsSection() {
   const currentPlayer = React.useContext(CurrentPlayerContext)
   const titleClasses = useTitleStyle()
   return (
-    <Grid item>
-      <Grid container spacing={2} direction="column">
+    <ExpansionPanel
+      style={{
+        boxShadow: "none",
+        background: "none"
+      }}
+    >
+      <ExpansionPanelSummary
+        expandIcon={<ExpandMoreIcon />}
+        style={{ padding: 0 }}
+        aria-controls="panel1a-content"
+        id="panel1a-header"
+      >
+        <Typography variant="h4" className={titleClasses.title}>
+          Settings
+        </Typography>
+      </ExpansionPanelSummary>
+      <ExpansionPanelDetails>
         <Grid item>
-          <Typography variant="h4" className={titleClasses.title}>
-            Settings
-          </Typography>
-          {currentPlayer.role === PlayerRole.Participant && (
-            <div style={{ color: grey[600] }}>
-              (Only your host can set these)
-            </div>
-          )}
+          <Grid container spacing={2} direction="column">
+            <Grid item>
+              {currentPlayer.role === PlayerRole.Participant && (
+                <div style={{ color: grey[600] }}>
+                  (Only your host can set these)
+                </div>
+              )}
+            </Grid>
+            <Grid item>
+              <SecondsPerTurnInput
+                value={String(currentGame.seconds_per_turn || "")}
+              ></SecondsPerTurnInput>
+            </Grid>
+            <Grid item>
+              <SubmissionsPerPlayerInput
+                value={String(currentGame.num_entries_per_player || "")}
+              ></SubmissionsPerPlayerInput>
+            </Grid>
+            <Grid item>
+              <LetterInput
+                value={currentGame.starting_letter || ""}
+              ></LetterInput>
+            </Grid>
+            <Grid item>
+              <Typography variant="h6" className={titleClasses.title}>
+                Rounds
+              </Typography>
+              <Box pl={2} pt={1} fontSize="0.75rem" color={grey[600]}>
+                {currentPlayer.role === PlayerRole.Host &&
+                  "You can add, remove, or reorder rounds. By default, cards submitted will be re-used across rounds of Taboo, Charades, and Password."}
+              </Box>
+            </Grid>
+            <Grid item>
+              {currentPlayer.role === PlayerRole.Host ? (
+                <ControllableRoundSettings></ControllableRoundSettings>
+              ) : (
+                <RoundSettings></RoundSettings>
+              )}
+            </Grid>
+            <Grid item></Grid>
+          </Grid>
         </Grid>
-        <Grid item>
-          <SecondsPerTurnInput
-            value={String(currentGame.seconds_per_turn || "")}
-          ></SecondsPerTurnInput>
-        </Grid>
-        <Grid item>
-          <Typography variant="h6" className={titleClasses.title}>
-            Cards
-          </Typography>
-        </Grid>
-        <Grid item>
-          <SubmissionsPerPlayerInput
-            value={String(currentGame.num_entries_per_player || "")}
-          ></SubmissionsPerPlayerInput>
-        </Grid>
-        <Grid item>
-          <LetterInput value={currentGame.starting_letter || ""}></LetterInput>
-        </Grid>
-        <Grid item>
-          <Typography variant="h6" className={titleClasses.title}>
-            Rounds
-          </Typography>
-          <Box pl={2} pt={1} fontSize="0.75rem" color={grey[600]}>
-            {currentPlayer.role === PlayerRole.Host &&
-              "You can add, remove, or reorder rounds. By default, cards submitted will be re-used across rounds of Taboo, Charades, and Password."}
-          </Box>
-        </Grid>
-        <Grid item>
-          {currentPlayer.role === PlayerRole.Host ? (
-            <ControllableRoundSettings></ControllableRoundSettings>
-          ) : (
-            <RoundSettings></RoundSettings>
-          )}
-        </Grid>
-        <Grid item></Grid>
-      </Grid>
-    </Grid>
+      </ExpansionPanelDetails>
+    </ExpansionPanel>
   )
 }
 
