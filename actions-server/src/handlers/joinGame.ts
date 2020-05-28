34298de8679bbb72c5701bab23e9a7a1cdecd8b3
Avi Moondra <avimoondra@gmail.com>
2020-05-28 10:09:53 -0700

    use typescript-graphql-request plugin for better express typing

--- actions-server/src/handlers/joinGame.ts
+++ actions-server/src/handlers/joinGame.ts
@@ -1,90 +1,73 @@
 import { Request, Response } from "express"
-import { DocumentNode, print as gqlToString } from "graphql"
 import jwt from "jsonwebtoken"
-import fetch from "node-fetch"
-import {
-  InsertPlayerForGame,
-  InsertPlayerForGameMutationVariables,
-  LookupPlayerForGame,
-  LookupPlayerForGameQueryVariables,
-} from "../generated/graphql"
-
-// execute the parent operation in Hasura
-const execute = async <T>(query: DocumentNode, variables: T) => {
-  const fetchResponse = await fetch(
-    process.env.HASURA_ENDPOINT || "missing endpoint",
-    {
-      method: "POST",
-      body: JSON.stringify({
-        query: gqlToString(query),
-        variables: variables,
-      }),
-    }
-  )
-  const data = await fetchResponse.json()
-  console.log("DEBUG: ", data)
-  return data
-}
+import { graphQLClient } from "../graphQLClient"
 
 // Request Handler
 const handler = async (req: Request, res: Response) => {
+  if (!process.env.HASURA_ENDPOINT || !process.env.HASURA_GRAPHQL_JWT_SECRET) {
+    return res.status(500)
+  }
+
+  const client = graphQLClient()
+
   // get request input
   const { gameId, clientUuid } = req.body.input
 
   // execute the Hasura operation(s)
   let playerId
-  const { data: lookupData, errors } = await execute<
-    LookupPlayerForGameQueryVariables
-  >(LookupPlayerForGame, {
-    gameId,
-    clientUuid,
-  })
-  if (errors) {
-    return res.status(400).json(errors[0])
-  }
-  if (lookupData.players[0]) {
-    // already joined game
-    playerId = lookupData.players[0].id
-  } else {
-    // new player for game
-    const { data: insertData, errors } = await execute<
-      InsertPlayerForGameMutationVariables
-    >(InsertPlayerForGame, {
+
+  try {
+    const { data, errors } = await client.LookupPlayerForGame({
       gameId,
       clientUuid,
     })
-    if (errors) {
-      return res.status(400).json(errors[0])
+    if (data?.players[0]) {
+      // already joined game
+      playerId = data.players[0].id
+    } else {
+      // new player for game
+      try {
+        const { data: data_insert, errors } = await client.InsertPlayerForGame({
+          gameId,
+          clientUuid,
+        })
+        if (data_insert?.insert_players_one) {
+          playerId = data_insert.insert_players_one.id
+        }
+      } catch {
+        return res.status(400)
+      }
     }
-    playerId = insertData.insert_players_one.id
+
+    if (!playerId || errors) {
+      return res.status(500).json({ success: false, errors })
+    }
+
+    const tokenContents = {
+      sub: playerId.toString(),
+      iat: Date.now() / 1000,
+      iss: "https://fishbowl-game.com/",
+      "https://hasura.io/jwt/claims": {
+        "x-hasura-allowed-roles": ["player", "anonymous"],
+        "x-hasura-user-id": playerId.toString(),
+        "x-hasura-default-role": "player",
+        "x-hasura-role": "player",
+      },
+      exp: Math.floor(Date.now() / 1000) + 24 * 60 * 60,
+    }
+
+    const token = jwt.sign(
+      tokenContents,
+      process.env.HASURA_GRAPHQL_JWT_SECRET || "missing secret"
+    )
+
+    return res.status(200).json({
+      id: playerId.toString(),
+      jwt_token: token,
+    })
+  } catch {
+    return res.status(400)
   }
-
-  if (!playerId) {
-    return res.status(500)
-  }
-
-  const tokenContents = {
-    sub: playerId.toString(),
-    iat: Date.now() / 1000,
-    iss: "https://fishbowl-game.com/",
-    "https://hasura.io/jwt/claims": {
-      "x-hasura-allowed-roles": ["player", "anonymous"],
-      "x-hasura-user-id": playerId.toString(),
-      "x-hasura-default-role": "player",
-      "x-hasura-role": "player",
-    },
-    exp: Math.floor(Date.now() / 1000) + 24 * 60 * 60,
-  }
-
-  const token = jwt.sign(
-    tokenContents,
-    process.env.HASURA_GRAPHQL_JWT_SECRET || "missing secret"
-  )
-
-  return res.json({
-    id: playerId.toString(),
-    jwt_token: token,
-  })
 }
 
 module.exports = handler
