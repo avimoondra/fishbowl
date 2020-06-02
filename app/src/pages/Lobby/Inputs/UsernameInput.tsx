fc6b1285554ad05e910e40b51174188a844d4dbc
Kolbe Bowring <kfbow@users.noreply.github.com>
2020-06-02 11:34:44 -0700

    update Lobby "Join" button to handle username changes (#90)
    Co-authored-by: Avi Moondra <avimoondra@gmail.com>

--- app/src/pages/Lobby/Inputs/UsernameInput.tsx
+++ app/src/pages/Lobby/Inputs/UsernameInput.tsx
@@ -5,16 +5,21 @@ import * as React from "react"
 function UsernameInput(props: { playerId: Players["id"]; username: string }) {
   const [updatePlayer] = useUpdatePlayerMutation()
   const [value, setValue] = React.useState(props.username || "")
+  const [savedName, setSavedName] = React.useState(value)
+
   return (
     <form
-      onSubmit={(event) => {
+      onSubmit={async (event) => {
         event.preventDefault()
-        updatePlayer({
+        const response = await updatePlayer({
           variables: {
             id: props.playerId,
             input: { username: value },
           },
         })
+        if (response.data?.update_players_by_pk?.username) {
+          setSavedName(value)
+        }
       }}
     >
       <Box display="flex" alignItems="flex-start" justifyContent="flex-start">
@@ -33,11 +38,11 @@ function UsernameInput(props: { playerId: Players["id"]; username: string }) {
         <Box pl={2}>
           <Button
             type="submit"
-            disabled={value === ""}
+            disabled={value === "" || value === savedName}
             variant="outlined"
             size="large"
           >
-            Join
+            {!!savedName ? "Change" : "Join"}
           </Button>
         </Box>
       </Box>
