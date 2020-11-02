c88344b15fb41a27280e36c2bca8a9a71fccf16c
Avi Moondra <avimoondra@gmail.com>
2020-11-02 15:08:54 -0500

    Revert "Revert "Add 🚀🦀 rocketcrab support (#110)" (#111)"
    This reverts commit 0de2f17dadf5a41ee635ffc3735853d3ea7a1d9d.

---
+++ actions-server/src/handlers/newGame.ts
@@ -0,0 +1,28 @@
+import { Request, Response } from "express"
+import { graphQLClient } from "graphQLClient"
+
+// Request Handler
+const handler = async (req: Request, res: Response) => {
+  // execute the Hasura operation(s)
+  const { data, errors: err1 } = await graphQLClient().StartGame()
+
+  if (!data?.insert_games_one?.id || err1) {
+    return res.status(400).json({ errros: err1 })
+  }
+
+  const { data: gameData, errors: err2 } = await graphQLClient().GameById({
+    id: data.insert_games_one.id,
+  })
+
+  const join_code = gameData?.games_by_pk?.join_code
+
+  if (!join_code || err2) {
+    return res.status(400).json({ errros: err2 })
+  }
+
+  return res.json({
+    join_code,
+  })
+}
+
+module.exports = handler
