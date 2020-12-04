1d9e4b6e538f372df04fc279afd214c018e89454
Avi Moondra <avimoondra@gmail.com>
2020-12-04 18:38:20 -0500

    provide settings summary prior to expanding (#121)

---
+++ app/src/pages/Lobby/SettingsSummary.tsx
@@ -0,0 +1,45 @@
+import { CurrentGameContext } from "contexts/CurrentGame"
+import { GameCardPlayStyleEnum } from "generated/graphql"
+import { startCase } from "lodash"
+import * as React from "react"
+
+function SettingsSummary() {
+  const currentGame = React.useContext(CurrentGameContext)
+  let cardPlayStyle = ""
+  if (
+    currentGame.card_play_style === GameCardPlayStyleEnum.PlayersSubmitWords
+  ) {
+    cardPlayStyle = `Players will submit ${
+      currentGame.num_entries_per_player
+    } words${
+      currentGame.starting_letter
+        ? `, all starting with the letter "${currentGame.starting_letter}."`
+        : "."
+    } ${
+      currentGame.screen_cards ? "The host will be able to screen words." : ""
+    }`
+  } else if (
+    currentGame.card_play_style === GameCardPlayStyleEnum.HostProvidesWords
+  ) {
+    cardPlayStyle = `The host will provide the words.`
+  }
+
+  return (
+    <>
+      {cardPlayStyle}
+      <br />
+      <br />
+      {`Turns will last ${
+        currentGame.seconds_per_turn
+      } seconds across rounds of ${currentGame.rounds
+        .map((round) => startCase(round.value))
+        .join(", ")}. ${
+        currentGame.allow_card_skips
+          ? "Players can skip cards during their turn."
+          : "Players cannot skip cards during their turn."
+      }`}
+    </>
+  )
+}
+
+export default SettingsSummary
