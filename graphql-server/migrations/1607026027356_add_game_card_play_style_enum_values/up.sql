16111087c3d32b262f868c4f9feaa8b57a987e61
Avi Moondra <avimoondra@gmail.com>
2020-12-04 11:07:58 -0500

    migrate to support different card submission play styles

---
+++ graphql-server/migrations/1607026027356_add_game_card_play_style_enum_values/up.sql
@@ -0,0 +1,1 @@
+INSERT INTO "public"."game_card_play_style"(value) VALUES ('players_submit_words'), ('host_provides_words'), ('pack_words');
---
+++ graphql-server/migrations/1607026722832_set_fk_public_games_card_play_style/down.sql
@@ -0,0 +1,1 @@
+alter table "public"."games" drop constraint "games_card_play_style_fkey";
