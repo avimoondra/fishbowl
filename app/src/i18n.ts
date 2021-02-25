5bbcfd6b59320945be5427bfe16d456af815e06d
Nick Amoscato <nick@amoscato.com>
2021-02-24 20:04:00 -0500

    Dynamically set version

--- app/src/i18n.ts
+++ app/src/i18n.ts
@@ -11,8 +11,9 @@ const locizeApiKey = development
   ? process.env.REACT_APP_FISHBOWL_LOCIZE_API_KEY
   : undefined
 const saveMissing = Boolean(development && locizeApiKey)
+const inContext = "1" === queryString.parse(window.location.search).locize
 
-if ("1" === queryString.parse(window.location.search).locize) {
+if (inContext) {
   i18n.use(locizePlugin)
 }
 
@@ -43,7 +44,7 @@ i18n
       apiKey: locizeApiKey,
       referenceLng: "en",
       // @see https://docs.locize.com/guides-tips-and-tricks/going-production#versions-and-caching
-      version: development ? "latest" : "production",
+      version: development || inContext ? "latest" : "production",
     },
   })
 
