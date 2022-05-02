860470385c32c8a65d81695685096d57e27d4f86
Nick Amoscato <nick@amoscato.com>
2022-05-02 19:23:11 -0400

    update `games` foreign key delete constraints

---
+++ graphql-server/migrations/1651533616427_set_fk_public_rounds_game_id/up.sql
@@ -0,0 +1,5 @@
+alter table "public"."rounds" drop constraint "rounds_game_id_fkey",
+             add constraint "rounds_game_id_fkey"
+             foreign key ("game_id")
+             references "public"."games"
+             ("id") on update restrict on delete cascade;
