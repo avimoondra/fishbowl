12468c864d4d025160d5cbfb7628e5d32c5629cc
Avi Moondra <avimoondra@gmail.com>
2022-05-03 07:50:45 -0700

    Revert "update `games` foreign key delete constraints (#154)"
    This reverts commit da76ec498e02b4ee462ad0767373db7c900d8314.

--- graphql-server/migrations/1651533479496_set_fk_public_players_game_id/up.sql
+++
@@ -1,5 +0,0 @@
-alter table "public"."players" drop constraint "players_game_id_fkey",
-             add constraint "players_game_id_fkey"
-             foreign key ("game_id")
-             references "public"."games"
-             ("id") on update restrict on delete cascade;
